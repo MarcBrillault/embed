57449c2228dbeace7471cfe7e674dfec7a3e9632
Marc Brillault <marc@brillault.fr>
2018-07-16 07:30:22 +0200

    Updated youtube regexp to include underscores

--- src/Classes/Video/MainStream/YouTube.php
+++ src/Classes/Video/MainStream/YouTube.php
@@ -7,9 +7,9 @@ use Embryo\Embed\Classes\Video;
 class YouTube extends Video
 {
     protected $regexp = [
-        '#^https?://www\.youtube\.com/watch\?v=([a-zA-Z0-9]+)#',
-        '#^https?://youtu\.be/([a-zA-Z0-9]+)#',
-        '#^https?://www\.youtube\.com/embed/([a-zA-Z0-9]+)#',
+        '#^https?://www\.youtube\.com/watch\?v=([_a-zA-Z0-9]+)#',
+        '#^https?://youtu\.be/([_a-zA-Z0-9]+)#',
+        '#^https?://www\.youtube\.com/embed/([_a-zA-Z0-9]+)#',
     ];
 
     protected $template = <<<'TEMPLATE'
